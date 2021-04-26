1c4512ee6a683fba2111e76da3c38123dc4b3f46
red-star25 <nakumdhruv123@gmail.com>
2021-04-26 14:11:05 +0530

    Image next added

--- component/Project/Project.jsx
+++ component/Project/Project.jsx
@@ -1,22 +1,23 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import styles from "../../styles/Project.module.css";
-// import netflix from "/images/netflix_clone.jfif";
-// import google from "/images/google_clone.jfif";
-// import spotify from "/images/spotify_clone.jfif";
+import Image from "next/image";
+// import netflix from "/images/netflix_clone.png";
+// import google from "/images/google_clone.png";
+// import spotify from "/images/spotify_clone.png";
 // import aiReader from "/images/ai_reader.png";
 // import budgetTracker from "/images/budget_tracker.png";
 // import forkify from "/images/forkify.png";
 // import pigGame from "/images/pig_game.png";
-// import todo from "/images/todo.jfif";
+// import todo from "/images/todo.png";
 
 // import workout from "/images/Workout.png";
 // import inbrief from "/images/inbrief.png";
 // import waller from "/images/waller.png";
 // import wyck from "/images/weather.png";
-// import whatsapp from "/images/whatsapp_mob.jfif";
-// import messanger from "/images/messanger_app.jfif";
-// import covidTracker from "/images/covid_tracker_mob.jfif";
-// import mediaPlayer from "/images/media_player_mob.jfif";
+// import whatsapp from "/images/whatsapp_mob.png";
+// import messanger from "/images/messanger_app.png";
+// import covidTracker from "/images/covid_tracker_mob.png";
+// import mediaPlayer from "/images/media_player_mob.png";
 
 import Aos from "aos";
 import "aos/dist/aos.css";
@@ -24,19 +25,24 @@ import "aos/dist/aos.css";
 function Project() {
   const webProjects = [
     {
+      title: "GhostDB",
+      subtitle: "A Video games discovery website",
+      image: "/images/ghost.png",
+    },
+    {
       title: "Netflix",
       subtitle: "With video functionality and authentication",
-      image: "/images/netflix_clone.jfif",
+      image: "/images/netflix_clone.png",
     },
     {
       title: "Google",
       subtitle: "Using Google Search API",
-      image: "/images/google_clone.jfif",
+      image: "/images/google_clone.png",
     },
     {
       title: "Spotify",
       subtitle: "Using Spotify Authentication and Media Player",
-      image: "/images/spotify_clone.jfif",
+      image: "/images/spotify_clone.png",
     },
     {
       title: "AI News Reader",
@@ -61,7 +67,7 @@ function Project() {
     {
       title: "Google Keeps Note",
       subtitle: "Daily notes taking web application",
-      image: "/images/todo.jfif",
+      image: "/images/todo.png",
     },
   ];
   const appProjects = [
@@ -88,22 +94,22 @@ function Project() {
     {
       title: "Whatsapp",
       subtitle: "An UI clone of Whatsapp mobile application",
-      image: "/images/whatsapp_mob.jfif",
+      image: "/images/whatsapp_mob.png",
     },
     {
       title: "Messanger",
       subtitle: "An UI clone of Facebook messanger mobile application",
-      image: "/images/messanger_app.jfif",
+      image: "/images/messanger_app.png",
     },
     {
       title: "Covid Tracker",
       subtitle: "App to track Covid-19 cases from all over the world",
-      image: "/images/covid_tracker_mob.jfif",
+      image: "/images/covid_tracker_mob.png",
     },
     {
       title: "Media Player Neumorphic Design",
       subtitle: "An Neumorphic design of Media Player ",
-      image: "/images/media_player_mob.jfif",
+      image: "/images/media_player_mob.png",
     },
   ];
 
@@ -117,11 +123,22 @@ function Project() {
       <div className={styles.project__web}>
         <h2>Website Development Projects</h2>
         <div className={styles.project__listOfProject}>
-          {webProjects.map((project) => {
+          {webProjects.map((project, index) => {
             return (
-              <div data-aos="zoom-in-up" className={styles.projectCard__main}>
+              <div
+                key={index}
+                data-aos="zoom-in-up"
+                className={styles.projectCard__main}
+              >
                 <div className={styles.projectCard}>
-                  <img src={project.image} alt="" />
+                  <Image
+                    width={240}
+                    height={140}
+                    layout="responsive"
+                    objectFit="cover"
+                    src={project.image}
+                    alt=""
+                  />
                 </div>
                 <div className={styles.projectCard__title}>
                   <h2>{project.title}</h2>
@@ -141,7 +158,13 @@ function Project() {
             return (
               <div data-aos="zoom-in-up" className={styles.projectCard__main}>
                 <div className={styles.projectCard}>
-                  <img src={project.image} alt="" />
+                  <Image
+                    width={240}
+                    height={140}
+                    objectFit="cover"
+                    src={project.image}
+                    alt=""
+                  />
                 </div>
                 <div className={styles.projectCard__title}>
                   <h2>{project.title}</h2>
